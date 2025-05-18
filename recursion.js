20bb74af6d3ae5eca2aa7f8dc268716dc2708200
Gaurav Bajaj <gauravbajaj@Gauravs-MacBook-Air.local>
2025-05-18 21:51:06 +0530

    add isPlaindrome using recursion

--- recursion.js
+++ recursion.js
@@ -7,17 +7,34 @@
 // }
 // console.log(sumRange(4)); // 10
 
-function someofDigits(...num) {
-  let t = 0;
+// function someofDigits(...num) {
+//   let t = 0;
 
-  function helperMethod(t, nums) {
-    if (nums.length === 0) {
-      return t;
+//   function helperMethod(t, nums) {
+//     if (nums.length === 0) {
+//       return t;
+//     }
+//     t += nums.shift();
+//     return helperMethod(t, nums);
+//   }
+
+//   return helperMethod(t, num.join("").split("").map(Number));
+// }
+// console.log(someofDigits(123456789)); //
+
+function isPalindrome(str) {
+  let p1 = 0;
+  let p2 = str.length - 1;
+  function helperMethod(p1, p2, str) {
+    if (p1 >= p2) {
+      return true;
     }
-    t += nums.shift();
-    return helperMethod(t, nums);
+    if (str[p1] !== str[p2]) {
+      return false;
+    }
+    return helperMethod((p1 = p1 + 1), (p2 = p2 - 1), str);
   }
 
-  return helperMethod(t, num.join("").split("").map(Number));
+  return helperMethod(p1, p2, str.toLowerCase());
 }
-console.log(someofDigits(123456789)); //
+console.log(isPalindrome("hello")); // true
