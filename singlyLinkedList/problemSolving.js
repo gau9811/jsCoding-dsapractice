739ff1d88c8c09b920e8a0500ddfe5ba8354aae3
Gaurav Bajaj <gauravbajaj@Gauravs-MacBook-Air.local>
2025-05-21 21:41:11 +0530

    change in problemsolving add

--- singlyLinkedList/problemSolving.js
+++ singlyLinkedList/problemSolving.js
@@ -8,7 +8,11 @@ class SinglyLinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
+    this.head1 = null;
+    this.tail1 = null;
+
     this.size = 0;
+    this.size1 = 0;
   }
 
   push(data) {
@@ -21,6 +25,18 @@ class SinglyLinkedList {
       this.tail = newNode;
     }
     this.size++;
+  }
+
+  push1(data) {
+    const newNode = new Node(data);
+    if (!this.head1) {
+      this.head1 = newNode;
+      this.tail1 = newNode;
+    } else {
+      this.tail1.next = newNode;
+      this.tail1 = newNode;
+    }
+    this.size1++;
   }
 
   pop() {
@@ -167,15 +183,89 @@ class SinglyLinkedList {
     }
     return this.getlist();
   }
+
+  mergeTwoSortedList() {
+    if (!this.head || !this.head1) return null;
+    let current1 = this.head;
+    let current2 = this.head1;
+    let newList = new SinglyLinkedList();
+    while (current1 && current2) {
+      if (current1.data < current2.data) {
+        newList.push(current1.data);
+        current1 = current1.next;
+      } else {
+        newList.push(current2.data);
+        current2 = current2.next;
+      }
+    }
+
+    while (current1) {
+      newList.push(current1.data);
+      current1 = current1.next;
+    }
+
+    // Add remaining nodes from current2
+    while (current2) {
+      newList.push(current2.data);
+      current2 = current2.next;
+    }
+    return newList;
+  }
+
+  addTwoNumber() {
+    let s1 = "";
+    let s2 = "";
+    let current1 = this.head;
+    let current2 = this.head1;
+    while (current1 && current2) {
+      s1 += current1.data;
+      current1 = current1.next;
+      s2 += current2.data;
+      current2 = current2.next;
+    }
+    s1 = `${parseInt(s1) + parseInt(s2)}`.split("");
+    let newList = new SinglyLinkedList();
+    let i = 0;
+    while (i < s1.length) {
+      newList.push(parseInt(s1[i]));
+      i++;
+    }
+    console.log(newList);
+    // return newList;
+
+    // here is the code to add two numbers represented by linked lists in reverse order
+    // let current1 = this.head;
+    // let current2 = this.head1;
+    // let carry = 0;
+
+    // let newList = new SinglyLinkedList();
+
+    // while (current1 || current2 || carry) {
+    //   let val1 = current1 ? current1.data : 0;
+    //   let val2 = current2 ? current2.data : 0;
+
+    //   let sum = val1 + val2 + carry;
+    //   carry = Math.floor(sum / 10);
+    //   newList.push(sum % 10);
+
+    //   if (current1) current1 = current1.next;
+    //   if (current2) current2 = current2.next;
+    // }
+
+    return newList;
+  }
 }
 
 const list = new SinglyLinkedList();
+
 list.push(1);
-list.push(2);
 list.push(3);
-list.push(4);
 list.push(5);
-list.push(6);
 list.push(7);
 
-console.log(list.swapNode());
+list.push1(2);
+list.push1(4);
+list.push1(6);
+list.push1(8);
+
+console.log(list.addTwoNumber());
