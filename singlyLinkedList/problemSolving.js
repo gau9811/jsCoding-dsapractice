6324a3b7202607bee27dbf418e6e69b101deb534
Gaurav Bajaj <Gaurav.Bajaj@smartenergywater.in>
2025-05-21 04:12:03 -0700

    add swapnode and palindrome and rotate and getlist

--- singlyLinkedList/problemSolving.js
+++ singlyLinkedList/problemSolving.js
@@ -72,7 +72,6 @@ class SinglyLinkedList {
     if (!this.head) return null;
     let slow = this.head;
     let fast = this.head;
-    let inc = 0;
     while (fast?.next) {
       slow = slow.next;
       fast = fast.next.next;
@@ -98,19 +97,16 @@ class SinglyLinkedList {
     if (!this.head) return null;
     let slow = this.head;
     let fast = this.head;
-    let newSlow = this.head;
     let currStr = [];
+
     while (fast?.next) {
+      currStr.push(slow.data);
       slow = slow.next;
       fast = fast.next.next;
     }
-    let middle = Math.floor(this.size / 2);
-    let i = 0;
 
-    while (i <= middle) {
-      currStr.push(newSlow.data);
-      newSlow = newSlow.next;
-      i++;
+    if (fast) {
+      slow = slow.next;
     }
 
     while (slow?.data) {
@@ -122,16 +118,64 @@ class SinglyLinkedList {
     }
     return true;
   }
+  rotate(ind) {
+    if (ind > this.size) return null;
+    let firstNode = this.head;
+    let remainingList = this.head.next;
+
+    let currentH = 0;
+    while (currentH < ind) {
+      firstNode.next = remainingList;
+      this.tail.next = firstNode;
+      firstNode.next = null;
+      this.head = remainingList;
+      this.tail = firstNode;
+      firstNode = this.head;
+      remainingList = this.head.next;
+      currentH++;
+    }
+  }
+
+  getlist() {
+    let current = this.head;
+    let list = [];
+    while (current) {
+      list.push(current.data);
+      current = current.next;
+    }
+    return list;
+  }
+
+  swapNode() {
+    if (!this.head) return null;
+    let current = this.head;
+    let prev = null;
+    let next = null;
+
+    while (current && current.next) {
+      next = current.next;
+      current.next = next.next;
+      next.next = current;
+
+      if (prev) {
+        prev.next = next;
+      } else {
+        this.head = next;
+      }
+      prev = current;
+      current = current.next;
+    }
+    return this.getlist();
+  }
 }
 
 const list = new SinglyLinkedList();
-list.push("h");
-list.push("e");
-list.push("l");
-list.push("l");
-list.push("o");
-// list.push("a");
-// list.push("r");
-// list.push(1);
-// list.pop();
-console.log(list.palindrome());
+list.push(1);
+list.push(2);
+list.push(3);
+list.push(4);
+list.push(5);
+list.push(6);
+list.push(7);
+
+console.log(list.swapNode());
