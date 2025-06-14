fe435d9d7d7694747e4b76821a0099a437e6dfc9
Gaurav Bajaj <gauravbajaj@Gauravs-MacBook-Air.local>
2025-06-14 19:49:32 +0530

    need to compelete permutation

--- permutation.js
+++ permutation.js
@@ -1,3 +1,24 @@
-function checkInclusion(s1, s2) {}
+function checkInclusion(s1, s2) {
+  let set = {};
+  let left = 0;
 
-console.log(checkInclusion("abc", "lecabee"));
+  for (let i = 0; i < s1.length; i++) {
+    if (!set[s1[i]]) {
+      set[s1[i]] = 1;
+    } else {
+      set[s1[i]]++;
+    }
+  }
+
+  for (let right = 0; right < s2.length; right++) {
+    while (Object.keys(set).length && set[s2[right]]) {
+      console.log(set);
+      set[s2[left]]--;
+      if (set[s2[left]] === 0) delete set[s2[left]];
+    }
+    left++;
+  }
+  console.log(set);
+}
+
+console.log(checkInclusion("abc", "lecaabee"));
