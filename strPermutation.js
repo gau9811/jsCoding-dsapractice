636d7d99d867f1bb304711daae04b2da34b49af5
Gaurav Bajaj <gauravbajaj@Gauravs-MacBook-Air.local>
2025-06-17 20:31:15 +0530

    need to compelete permutation tommorrow

--- strPermutation.js
+++ strPermutation.js
@@ -22,3 +22,24 @@ var checkInclusion = function (s1, s2) {
   }
 };
 console.log(checkInclusion("ab", "eidbaooo"));
+
+// 567. Permutation in String
+// Attempted
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+
+// In other words, return true if one of s1's permutations is the substring of s2.
+
+// Example 1:
+
+// Input: s1 = "ab", s2 = "eidbaooo"
+// Output: true
+// Explanation: s2 contains one permutation of s1 ("ba").
+// Example 2:
+
+// Input: s1 = "ab", s2 = "eidboaoo"
+// Output: false
