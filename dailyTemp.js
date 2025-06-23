a68b900b9677808f96dc851cc0ceb56948aa9737
Gaurav Bajaj <gauravbajaj@Gauravs-MacBook-Air.local>
2025-06-23 19:21:02 +0530

    dailyTemp

--- dailyTemp.js
+++ dailyTemp.js
@@ -1,19 +1,18 @@
 function dailyTemperatures(temperatures) {
   let stack = [];
-  let count = 0;
-  let i = 0;
-  let j = 1;
-  while (i <= temperatures.length) {
-    while (j <= temperatures.length) {
-        if(temperatures[j] > temperatures[i]){
-            i++
-            j = i
-            break
-        }else{
-            stack.push(temperatures[j])
-        }
-      j++ 
+  let res = new Array(temperatures.length).fill(0);
+
+  for (let i = 0; i < temperatures.length; i++) {
+    while (
+      stack.length > 0 &&
+      temperatures[i] > temperatures[stack[stack.length - 1]]
+    ) {
+      let resP = stack.pop();
+      res[resP] = i - resP;
     }
+
+    stack.push(i);
   }
+  console.log(res);
 }
 console.log(dailyTemperatures([30, 38, 30, 36, 35, 40, 28]));
